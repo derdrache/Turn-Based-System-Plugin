21c29013919da6d966e8cf6b325a1ad14bebcebc
DevDrache <dominik.mast.11@gmail.com>
2024-12-04 12:55:37 +0200

    clean code

--- addons/Turn_Based_System/test/move_button.gd
+++ addons/Turn_Based_System/test/move_button.gd
@@ -11,4 +11,3 @@ func _ready() -> void:
 
 func _on_pressed() -> void:
 	var turnBasedController: TurnBasedController = get_tree().get_first_node_in_group("turnBasedController")
-	print(turnBasedController.get_active_character().name)
