cd70340cfe72cf17e8991dcded2811ce236087bc
DevDrache <dominik.mast.11@gmail.com>
2024-12-19 16:17:19 +0200

    Update command_resource.gd

--- addons/Turn_Based_System/resources/command_resource.gd
+++ addons/Turn_Based_System/resources/command_resource.gd
@@ -2,11 +2,13 @@ extends Resource
 class_name CommandResource
 
 enum Target_Type {ENEMIES, PLAYERS, SELF}
+enum Command_Type {DAMAGE, HEAL, SUPPORT}
 
 @export var name : String
 
 ##Command type is used to determine which group the ability can be targeted at
 @export var targetType: Target_Type
+@export var commandType: Command_Type
 
 ## TargetCount specify how many targets the command should affect [br]
 ## -1: affect all
